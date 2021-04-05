478be4ebfbe90996de6263932db4df248a320ba2
MalikaAsh <61599833+MalikaAsh@users.noreply.github.com>
2021-04-05 11:04:39 +0500

    first commit - mvc application with DAL and MVC controllers

---
+++ FilmReviewApplication/FilmReviewApplication/Controllers/ReviewsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FilmReviewApplication.DAL;
+using FilmReviewApplication.Models;
+
+namespace FilmReviewApplication.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly FilmReviewApplicationDbContext _context;
+
+        public ReviewsController(FilmReviewApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+            var filmReviewApplicationDbContext = _context.Reviews.Include(r => r.Film).Include(r => r.User);
+            return View(await filmReviewApplicationDbContext.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Film)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            ViewData["FilmId"] = new SelectList(_context.Films, "Id", "CountryofProduction");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ReviewText,FilmId,UserId,Rating")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FilmId"] = new SelectList(_context.Films, "Id", "CountryofProduction", review.FilmId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", review.UserId);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["FilmId"] = new SelectList(_context.Films, "Id", "CountryofProduction", review.FilmId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", review.UserId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ReviewText,FilmId,UserId,Rating")] Review review)
+        {
+            if (id != review.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FilmId"] = new SelectList(_context.Films, "Id", "CountryofProduction", review.FilmId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", review.UserId);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Film)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Reviews.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FilmReviewApplication/FilmReviewApplication/DAL/FilmReviewApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FilmReviewApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmReviewApplication.DAL
+{
+
+    public class FilmReviewApplicationDbContext :DbContext
+    {
+        public FilmReviewApplicationDbContext(DbContextOptions<FilmReviewApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Film> Films { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+    }
+}
